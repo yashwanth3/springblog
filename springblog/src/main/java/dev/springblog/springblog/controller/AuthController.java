5633296bc83699b79ecf9481469f7a385dbcd5d6
yashwanth3 <yashwanthvemuganti@gmail.com>
2023-04-26 23:36:24 -0400

    registration part 1

---
+++ springblog/src/main/java/dev/springblog/springblog/controller/AuthController.java
@@ -0,0 +1,14 @@
+package dev.springblog.springblog.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    public vois singup(){
+
+    }
+}
